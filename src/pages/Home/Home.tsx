7ce7901e80b94989c832fcb73bfe6f7e365c43f9
denizafontineles <denizafontineles@outlook.com>
2024-05-10 10:11:02 -0300

    adiciona dependencies para utilizar no projeto

---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,17 @@
+import './Home.css';
+
+interface minhaProps {
+    title: string;
+    description: string;
+  }
+  
+function Home(props:minhaProps){
+    return (
+        <>
+        <h2>{props.title}</h2>
+        <p>{props.description}</p>
+        </>
+    );
+}
+
+export default Home;
